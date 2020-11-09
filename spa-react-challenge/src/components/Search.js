7dcf2455c347940feb4c430f8c873aeec6d361d1
Dione Cezar <dione_cezar@hotmail.com>
2020-11-09 19:07:21 -0200

    Correções, melhorias e busca de personagens

--- spa-react-challenge/src/components/Search.js
+++ spa-react-challenge/src/components/Search.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Search(props) {
+    const [value, setValue] = useState();
+
+    function execute(event) {
+        if (event.key === "Enter") {
+            value && props.onKeyDown && props.onKeyDown(value);
+        }
+    }
+
     return (
         <input 
             type="search"
             placeholder="Procure por heróis"
-            //onChange={data => { console.log(data.target.value) }}
-            style={props.style}>
+            onChange={data => setValue(data.target.value)}
+            style={props.style}
+            onKeyDown={event => execute(event)}>
         </input>
     );
 }
--- spa-react-challenge/src/pages/home.js
+++ spa-react-challenge/src/pages/home.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from '../components/Search';
+import { HttpGet } from '../components/HttpRequest';
 
 import Logo from '../assets/logo/Group.png';
 import Favorite from '../assets/icones/heart/Path.png';
 
 export default function Home() {
+    const [heroes, setHeroes] = useState([]);
+
+    const findHeroes = (stringToFind) => {
+        HttpGet(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${stringToFind}`)
+            .then(result => setHeroes(result.data.data.results))
+            .catch(error => console.error(error));
+    }
+
     return (
         <div className="main">
             <img src={Logo} alt="Logo Marvel" style={{ margin: '30px 0px' }} />
             <h1>EXPLORE O UNIVERSO</h1>
             <p>Mergulhe no domínio deslumbrante dos todos os personagens clássicos que você ama - e aqueles que você descobirá em breve!</p>
-            <Search style={{ backgroundColor: '#fdecec' , margin: '30px 0px', width: '600px' }}></Search>
+            <Search style={{ backgroundColor: '#fdecec', margin: '30px 0px', width: '80%' }} onKeyDown={findHeroes}></Search>
 
             <div className="top-panel">
-                <span>Encontrados 5 heróis</span>
+                <span>{`Encontrados ${heroes.length} heróis`}</span>
             </div>
 
-            <div className="list">
+            <div className="heroes-list">
                 {
-                    ['Hulk', 'Thor', 'Ironman', 'Spiderman', 'Ant-man'].map(item =>
-                        <div className="item">
-                            <img src="https://i2.wp.com/2centavos.com.br/wp-content/uploads/2013/05/thor.jpg" alt={item} />
-                            <p className="item-name">
-                                <span>{item}</span>
-                                <img src={Favorite} alt="Favoritar" onClick={() => {}} />
-                            </p>
+                    heroes.map(hero =>
+                        <div key={hero.id} className="hero-block" onClick={() => alert(`Detalhes: ${hero.id}`)}>
+                            <div className="hero-thumbnail" style={{ backgroundImage: `url(${hero.thumbnail.path}.${hero.thumbnail.extension})` }}></div>
+                            <div className="hero-name">
+                                <img src={Favorite} onClick={event => { alert(`Favorito: ${hero.id}`); event.stopPropagation(); }} alt="Favoritar" />
+                                <p>{hero.name}</p>
+                            </div>
                         </div>
                     )
                 }
