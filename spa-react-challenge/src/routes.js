93474ddeda08eb39297bb35e115c40fe7affa126
Dione Cezar <dione_cezar@hotmail.com>
2020-11-16 03:16:36 -0200

    Correção nome arquivos

--- spa-react-challenge/src/routes.js
+++ spa-react-challenge/src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Details from './pages/Details';
+import Home from './pages/home';
+import Details from './pages/details';
 
 export default function Routes() {
     return (
